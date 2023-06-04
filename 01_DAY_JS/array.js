d6bf375eb5c3f00bae9030668e87e17b9b739593
Ranjeet <sranjeet434@gmail.com>
2023-06-04 19:10:07 +0530

    js lec 2

---
+++ 01_DAY_JS/array.js
@@ -0,0 +1,12 @@
+let name=["ranjeet","markzuberker","sundarpichai","billgates"];
+console.log(name[0]);
+
+let BankAcount={
+    firstname:"Tokyo",
+    lastname:"Rio",
+    mobileNo:123456789,
+    balance:9999999999,
+};
+
+console.log(BankAcount);
+console.log(BankAcount.balance);
---
+++ 01_DAY_JS/date_and_maths.js
@@ -0,0 +1,26 @@
+const now=new Date();
+console.log(now);
+
+const date=new Date();
+console.log(date.toString());
+console.log(date.getFullYear());
+console.log(date.getMonth());
+
+
+// Maths
+
+const PI=Math.PI;
+console.log(PI);
+
+console.log(Math.round(PI));
+console.log(Math.floor(9.5));
+console.log(Math.ceil(9.5));
+
+
+// min and max
+
+console.log(Math.min(1,3,5,6,2));
+console.log(Math.max(3,4,2,6,5,1));
+
+// random 0 to 0.999999
+console.log(Math.random()*10);
