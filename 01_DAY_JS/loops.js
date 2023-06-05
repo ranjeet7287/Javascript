e7cf10c82e4e6781806c1562bdf342d41751c85e
Ranjeet <sranjeet434@gmail.com>
2023-06-05 08:51:26 +0530

    Day 1 folder done

---
+++ 01_DAY_JS/loops.js
@@ -0,0 +1,20 @@
+// do while , while , for
+
+// do -> program will execute atlest once 
+let i=6;
+do{
+    console.log('Value of i is :',i);
+    i++;
+}while(i<=5);
+
+// while
+let j=6;
+while(j<=5){
+    console.log('Value of j is :',j);
+    j++;
+}
+
+// for
+for(let i=0;i<=5;i++){
+    console.log(i);
+}
