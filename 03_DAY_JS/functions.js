3de0d4f2a489fddabb7a535886e160757173027f
Ranjeet <sranjeet434@gmail.com>
2023-06-07 20:05:27 +0530

    function in js

---
+++ 03_DAY_JS/functions.js
@@ -0,0 +1,63 @@
+// functions
+
+// Normal function
+// function sum(){
+//     let a=1;
+//     let b=2;
+//     console.log("sum of 1 + 2 is",a+b);
+// }
+
+// Function with parameter
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(4,5);
+
+
+// let a="ranjeet";
+// let b=7;
+// console.log(a+b);
+
+// function with return type
+// function sum(val1,val2){
+//     let result=val1+val2;
+//     return result;
+// }
+// let add=sum(12,12);
+// console.log(add);
+
+// let add2=sum(13,24);
+// console.log(add2);
+
+
+// string
+// function URL(url,domain){
+//     let con="http://";
+//     let result=con+url+domain;
+//     return result;
+// }
+// let url=URL("ranjeet",".me");
+// console.log(url);
+
+
+
+// function sum(arr){
+//     let sum=0;
+//     for(let i of arr){
+//         sum=sum+i;
+//     }
+//     return sum;
+// }
+// let sumarray=[1,2,3,4,5,6];
+// console.log(sum(sumarray));
+
+// function sum1(arr){
+//     let sum=0;
+//     for(let i=0;i<arr.length;i++){
+//         sum=sum+arr[i];
+//     }
+//     return sum;
+// }
+// console.log(sum1(sumarray));
+
+
