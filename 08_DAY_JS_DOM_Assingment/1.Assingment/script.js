ffe89a96c04a7c827d2046d738c7d783063575b7
Ranjeet <sranjeet434@gmail.com>
2023-06-30 22:39:56 +0530

    task 1 done

---
+++ 08_DAY_JS_DOM_Assingment/1.Assingment/script.js
@@ -0,0 +1,21 @@
+// Task1
+//  I have to add an element named "Hire Me" in the navigation menu.
+let mylist=document.querySelector('nav ul');
+let hireli=document.createElement("li");
+hireli.innerHTML="<a> Hire Me </a>";
+mylist.appendChild(hireli);
+
+
+
+// Task2 
+// I have to change the text inside search input field from "Search" to "Search My Project". 
+
+
+
+// Task3 I have to change the paragraph text from "a Freelancer for National and International Client" to "an Employee for iNeuron Intelligence Pvt Ltd".
+
+
+// Task4 I have to change to image to Hitesh sir's image.
+
+
+// Task5 I have to add another named "Support Me" beside the "Chat With Me" button.
