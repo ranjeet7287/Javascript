16a2a08529d02ffaae6d059923d55c009442c9ef
Ranjeet <sranjeet434@gmail.com>
2023-07-01 13:28:18 +0530

    assingment 4

---
+++ 08_DAY_JS_DOM_Assingment/4.Assingment/script.js
@@ -0,0 +1,21 @@
+let barbarian=document.querySelector('.clash-card__unit-stats--barbarian');
+barbarian.style.backgroundColor="#f5a33f";
+
+let statval=document.querySelectorAll('.stat-value,.stat');
+statval.forEach((element)=>{
+    element.style.color="white";
+})
+
+let Archer=document.querySelector('.clash-card__unit-stats--archer');
+Archer.style.backgroundColor="#da668d";
+
+let Giant=document.querySelector('.clash-card__unit-stats--giant');
+Giant.style.backgroundColor="#f5a33f";
+
+let goblin=document.querySelector('.clash-card__unit-stats--goblin');
+goblin.style.backgroundColor="#92cb42";
+
+let wizard=document.querySelector('.clash-card__unit-stats--wizard');
+wizard.style.backgroundColor="#f5a33f";
+
+
