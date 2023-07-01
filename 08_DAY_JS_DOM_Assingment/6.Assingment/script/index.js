cc6ff32635febf24f9447d309bd6df160cd429a8
Ranjeet <sranjeet434@gmail.com>
2023-07-01 15:31:42 +0530

    Assingment 6 done

---
+++ 08_DAY_JS_DOM_Assingment/6.Assingment/script/index.js
@@ -0,0 +1,8 @@
+// taks1 changeing logo of img
+let logoimg=document.querySelector('.logo');
+logoimg.src='./assets/ineuron-logo.png';
+
+
+// taks 2 I have to change app price from "$4" to "$10".
+let price=document.querySelector('.app_price span');
+price.innerText='$10';
