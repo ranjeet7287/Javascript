6da97f16bc2b2eaaaae6f65aaafff4a5878055e0
Ranjeet <sranjeet434@gmail.com>
2023-07-08 09:16:46 +0530

    fetch api

---
+++ 12_DAY_JS/script.js
@@ -0,0 +1,6 @@
+// fetch api
+const url="https://restcountries.com/v2/all"; // countries
+fetch(url)
+    .then((response)=>{response.json()})
+    .then((data)=>{console.log(data)})
+    .then((error)=>{console.log(error)});
